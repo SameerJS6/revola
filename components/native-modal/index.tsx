ce591662e567187ddbe057437831848ad6868ff3
SameerJS6 <singh6.sameer@gmail.com>
2025-03-11 00:10:40 +0530

    refactor: `NativeModalContent` & `NativeModaClose` to use Primitive's from origin packages.
    style: fix styling for header & footer

--- components/native-modal/index.tsx
+++ components/native-modal/index.tsx
@@ -3,9 +3,15 @@
 import * as React from "react";
 import * as DialogPrimitive from "@/components/ui/dialog";
 import * as DrawerPrimitive from "@/components/ui/drawer";
+import { DialogContent as RadixDialogContent } from "@radix-ui/react-dialog";
+import { Content as VaulDrawerContent } from "vaul";
 import useMediaQuery from "@/hooks/use-media-query";
 import { cn } from "@/lib/utils";
 import { X } from "lucide-react";
+
+// Todo:
+
+// 1. Manage vertical margin of content on drawer.
 
 type DrawerType = React.ComponentProps<typeof DrawerPrimitive.Drawer>;
 
@@ -37,7 +43,7 @@ const NativeModalOverlay = ({ className, ...props }: React.ComponentProps<typeof
     <NativeModalOverlay
       {...props}
       className={cn(
-        "fixed inset-0 z-50 bg-black/80 sm:data-[state=open]:animate-in sm:data-[state=closed]:animate-out sm:data-[state=closed]:fade-out-0 sm:data-[state=open]:fade-in-0",
+        "fixed inset-0 z-20 bg-black/40 md:data-[state=open]:animate-in md:data-[state=closed]:animate-out md:data-[state=closed]:fade-out-0 md:data-[state=open]:fade-in-0",
         className
       )}
     />
@@ -51,21 +57,25 @@ const NativeModalContent = ({
   ...props
 }: React.ComponentProps<typeof DialogPrimitive.DialogContent>) => {
   const isMobile = useMediaQuery("(min-width: 768px)");
-  const NativeModalContent = isMobile ? DialogPrimitive.DialogContent : DrawerPrimitive.DrawerContent;
+  const NativeModalContent = isMobile ? RadixDialogContent : VaulDrawerContent;
   return (
     <NativeModalPortal>
       <NativeModalOverlay />
       <NativeModalContent
         {...props}
         className={cn(
-          // "fixed z-50 bg-background",
-          // isMobile
-          //   ? "left-[50%] top-[50%] grid w-[calc(100%-2rem)] max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 rounded-lg border p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%]"
-          //   : "inset-x-0 bottom-0 mt-24 flex h-auto flex-col rounded-t-[10px] border",
+          "fixed z-50 bg-background",
+          isMobile
+            ? "left-[50%] top-[50%] grid h-auto max-h-[min(640px,80vh)] w-[calc(100%-2rem)] max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 rounded-lg border p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%]"
+            : "inset-x-0 bottom-0 mt-24 flex h-fit max-h-[80%] flex-col rounded-t-[10px] border pt-4",
           className
         )}
       >
-        {/* {!isMobile && <div className="mx-auto mt-4 h-2 w-[100px] rounded-full bg-muted" />} */}
+        {!isMobile && <div className="mx-auto mb-4 h-1.5 w-12 rounded-full bg-muted-foreground/25 dark:bg-muted" />}
+        <NativeModalClose className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity focus:outline-none focus:ring focus:ring-ring disabled:pointer-events-none">
+          <X className="size-4" />
+          <span className="sr-only">close</span>
+        </NativeModalClose>
         {children}
       </NativeModalContent>
     </NativeModalPortal>
@@ -73,36 +83,22 @@ const NativeModalContent = ({
 };
 NativeModalContent.displayName = "NativeModalContent";
 
-const NativeModalClose = ({
-  children,
-  className,
-  ...props
-}: React.ComponentProps<typeof DialogPrimitive.DialogClose>) => {
+const NativeModalClose = ({ ...props }: React.ComponentProps<typeof DialogPrimitive.DialogClose>) => {
   const isMobile = useMediaQuery("(min-width: 768px)");
   const NativeModalClose = isMobile ? DialogPrimitive.DialogClose : DrawerPrimitive.DrawerClose;
-  return (
-    <NativeModalClose {...props} className={cn("hover:opacity-100", className)}>
-      {!children ? (
-        <span className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity focus:outline-none focus:ring focus:ring-ring disabled:pointer-events-none">
-          <X className="size-4" />
-        </span>
-      ) : (
-        children
-      )}
-    </NativeModalClose>
-  );
+  return <NativeModalClose {...props} />;
 };
 NativeModalClose.displayName = "NativeModalClose";
 
 const NativeModalHeader = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => {
-  return <div className={cn("flex flex-col gap-1.5 p-4 text-center sm:p-0 sm:text-left", className)} {...props} />;
+  return <div className={cn("flex flex-col gap-1.5 p-4 text-center md:p-0 md:text-left", className)} {...props} />;
 };
 NativeModalHeader.displayName = "NativeModalHeader";
 
 const NativeModalFooter = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => {
   return (
     <footer
-      className={cn("flex flex-col gap-4 p-4 max-sm:mt-auto sm:flex-row sm:justify-end sm:gap-2 sm:p-0", className)}
+      className={cn("flex flex-col gap-4 p-4 max-md:mt-auto md:flex-row md:justify-end md:gap-2 md:p-0", className)}
       {...props}
     />
   );
