c364b307d0f405a9cd3c03190050165c67ded0d4
SameerJS6 <singh6.sameer@gmail.com>
2025-06-24 01:44:32 +0530

    refactor: update import paths in rehype-component.ts to align with new directory structure and enhance clarity

--- lib/rehype-component.ts
+++ lib/rehype-component.ts
@@ -28,11 +28,7 @@ export function rehypeComponent() {
           const filePath = src;
           let source = fs.readFileSync(filePath, "utf8");
 
-          source = source.replaceAll("react-payment-inputs/lib/images/index.js", "react-payment-inputs/images");
-          source = source.replaceAll(
-            "// Using this import \(ES import\) to fix the `ERR_UNSUPPORTED_DIR_IMPORT` error from `rehype-component\.ts`, as it throws an error while parsing all imports at run/build time\. This workaround specifically addresses the Node\.js ES modules limitation\.",
-            ""
-          );
+          source = source.replaceAll("registry/revola", "components/ui/revola");
           source = source.replaceAll("export default", "export");
 
           // Add code as children so that rehype can take over at build time.
